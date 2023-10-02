c28e72d06d277b31dc94250f8357da1e2e0d4f19
nikolajsvv <nikolajsvv@gmail.com>
2023-10-02 08:54:14 -0400

    feat: update about seciton

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,13 +6,8 @@ import { useEffect, useState, useRef } from 'react';
 
 export default function AboutPage() {
 	const [showSplash, setShowSplash] = useState(true);
-	const leftTextRef = useRef<HTMLHeadingElement>(null);
-	const rightTextRef = useRef<HTMLHeadingElement>(null);
-
-	const leftTexts = ['hello', 'I am'];
-	const rightTexts = ['sveiks', 'Nikolajs'];
-	let leftIndex = 0;
-	let rightIndex = 0;
+	const helloRef = useRef(null);
+	const sveiksRef = useRef(null);
 
 	useEffect(() => {
 		anime({
@@ -30,45 +25,24 @@ export default function AboutPage() {
 		return () => clearTimeout(timeoutId);
 	}, []);
 
+	// New useEffect to handle animations for hello and sveiks
 	useEffect(() => {
 		if (!showSplash) {
-			const swapTexts = () => {
-				// First, let's hide the text
-				anime({
-					targets: [leftTextRef.current, rightTextRef.current],
-					translateY: [0, -100],
-					opacity: [1, 0],
-					duration: 1500,
-					easing: 'easeOutExpo',
-					complete: function () {
-						if (leftTextRef.current && rightTextRef.current) {
-							// Change the text here when it's invisible
-							leftTextRef.current.innerHTML = leftTexts[leftIndex];
-							rightTextRef.current.innerHTML = rightTexts[rightIndex];
-							leftIndex = (leftIndex + 1) % leftTexts.length;
-							rightIndex = (rightIndex + 1) % rightTexts.length;
-						}
-						// Then, let's show the text
-						anime({
-							targets: [leftTextRef.current, rightTextRef.current],
-							translateY: [-100, 0],
-							opacity: [0, 1],
-							duration: 1500,
-							easing: 'easeOutExpo',
-						});
-					},
-				});
-			};
-			swapTexts(); // Run once immediately
-			const intervalId = setInterval(swapTexts, 5000); // Run every 5 seconds
-
-			return () => clearInterval(intervalId);
+			anime({
+				targets: [helloRef.current, sveiksRef.current],
+				translateY: [-100, 0],
+				opacity: [0, 1],
+				duration: 1500,
+				easing: 'easeOutExpo',
+				delay: anime.stagger(200),
+			});
 		}
-	}, [showSplash]);
+	}, [showSplash]); // Dependency array ensures this useEffect runs when showSplash changes
 
 	return (
-		<div className='h-screen flex items-center justify-center'>
+		<div className='h-screen flex'>
 			{showSplash ? (
+				// Splash Screen
 				<div className='flex h-screen w-full items-center justify-center bg-gray-900'>
 					<Image
 						src={currentProfileImg}
@@ -79,32 +53,26 @@ export default function AboutPage() {
 					/>
 				</div>
 			) : (
-				<div className='flex flex-col md:flex-row w-full justify-center items-center space-y-4 md:space-x-4 md:space-y-0'>
-					<div className='flex-1 flex justify-end items-center'>
-						<h1
-							ref={leftTextRef}
-							className='text-6xl md:text-7xl lg:text-8xl font-bold '
-						>
-							hello
-						</h1>
-					</div>
-					<div className='flex-initial'>
-						<Image
-							src={currentProfileImg}
-							alt={'Image of Nikolajs'}
-							width={300}
-							height={300}
-							className='hidden md:inline w-48 md:w-64 lg:w-auto z-10'
-						/>
-					</div>
-					<div className='flex-1 flex justify-start items-center'>
-						<h1
-							ref={rightTextRef}
-							className='text-6xl md:text-7xl lg:text-8xl font-bold text-gray-500 '
-						>
-							sveiks
-						</h1>
-					</div>
+				<div className='flex flex-col md:flex-row w-full justify-center items-center'>
+					<h1
+						ref={helloRef}
+						className='text-6xl md:text-8xl font-bold mb-4 md:mb-0 md:translate-x-12'
+					>
+						hello
+					</h1>
+					<Image
+						src={currentProfileImg}
+						alt={'Image of Nikolajs'}
+						width={300}
+						height={300}
+						className='hidden md:inline z-10'
+					/>
+					<h1
+						ref={sveiksRef}
+						className='text-6xl md:text-8xl font-bold text-gray-500 mt-4 md:mt-0 md:-translate-x-12'
+					>
+						sveiks
+					</h1>
 				</div>
 			)}
 		</div>
